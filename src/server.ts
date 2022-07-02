5f497f43c1571fefc5707107e2a24e36c45a2fe8
Chloé <cfontaine@citycable.ch>
2022-07-02 18:15:41 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,44 @@
 // Importing module
-import express from 'express';
-import bodyParser from 'body-parser';
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import express from "express";
+import bodyParser from "body-parser";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const app = express();
-const PORT:Number=3000;
+const PORT: Number = 3000;
 
 app.use(bodyParser.json());
 
 // Handling GET / Request
-app.get('/', (req, res) => {
-	res.send('Welcome to typescript backend!');
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to typescript backend!");
+});
 
-app.post('/login', (req, res) => {
-    console.log(req.body.username);
-    res.status(200).send();
-})
+app.post("/login", (req, res) => {
+  console.log(req.body.username);
+  res.status(200).send();
+});
 
-app.post('/register', async (req, res) => {
-    await prisma.$connect();
-    await prisma.user.create({
-		data:{
-			firstname: "Luca",
-			lastname: "Coduri",
-			username: "bafana",
-			email: "luca@gmail.com",
-			password: "banane"
-		}
-	})
+app.post("/register", async (req, res) => {
+  await prisma.$connect();
+  const user = await prisma.user.create({
+    data: {
+      firstname: "Luca",
+      lastname: "Coduri",
+      username: "bafana",
+      email: "luca@gmail.com",
+      password: "banane",
+    },
+  });
 
-	prisma.$disconnect;
-	// res.json(user);
-	res.send("coucou");
-})
+  prisma.$disconnect;
+  // res.json(user);
+  res.json(user);
+});
 
 // Server setup
-app.listen(PORT,() => {
-	console.log('The application is listening '
-		+ 'on port http://localhost:'+PORT);
-})
+app.listen(PORT, () => {
+  console.log(
+    "The application is listening " + "on port http://localhost:" + PORT
+  );
+});
